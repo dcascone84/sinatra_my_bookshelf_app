e9633ad3baa27981a4c8cfb25fba2169a9b44ac6
cdominick091984 <cdominick091984@gmail.com>
2020-06-25 21:58:38 -0400

    add new book form,  post books, and books show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,32 @@
 class BooksController < ApplicationController
 
     # line 8 - get books/new to render a form to create a new book
-    # post books receives the form and creates a new book with the imfomation entered
+    # line 12 - post books receives the form and creates a new book with the imfomation entered
     # show route for books
     # index route displays all books
 
     get '/book/new' do
+        # need to check if logged in
         erb :'/books/new'
     end
 
     post '/books' do
         # the params look like: 
         # {"title"=>"a title", "author"=>"the author", "genre"=>"the genre"}
-        # binding.pry
+        if !logged_in?
+            redirect :'/'
+        end
+
+        if params[:title] != "" && params[:author] != "" && params[:genre] != ""
+            @book = Book.create(title: params[:title], author: params[:author], genre: params[:genre], user_id: current_user.id)
+            redirect :"/books/#{@book.id}"
+        else
+            redirect :'/books/new'
+        end
     end
+
+    get "/books/:id" do
+        @book = Book.find_by(id: params[:id])
+        erb  :'/books/show'
+      end
 end
