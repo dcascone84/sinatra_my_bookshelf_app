1d65f0d8f8884935f8783ef834117347a5ab93e4
cdominick091984 <cdominick091984@gmail.com>
2020-06-28 10:51:33 -0400

    clean up the code by removing old comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "19ZlhdXk10HUjInPwxn6NErhTrlygQzKR9h6OFA8bnz96h6CSf1yv10Aqk5lnsyO4SKCn7pYG0K8pCMxC3AWptg" 
+    set :session_secret, ENV['SESSION_SECRET'] 
   end
 
   get '/' do
@@ -16,6 +16,8 @@ class ApplicationController < Sinatra::Base
       erb :welcome
     end
   end
+
+  
 
   helpers do
     
