a7b4421d549b36ebd5cb6fd1cb173b56eef7c0b7
cdominick091984 <cdominick091984@gmail.com>
2020-06-28 21:54:59 -0400

    update the root route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,14 +10,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    if logged_in?
-      redirect "users/#{current_user.id}"
-    else
       erb :welcome
-    end
   end
-
-  
 
   helpers do
     
